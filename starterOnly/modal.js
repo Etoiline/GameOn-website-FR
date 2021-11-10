8515af91e07ad71021111e279e1a8ef32cc32c39
Laura Casteran <laurac644@gmail.com>
2021-11-10 15:09:07 +0100

    Correction HTML Fonction quitter

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,4 @@
+/*fonction affichant le menu sur mobile*/
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -8,16 +9,24 @@ function editNav() {
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
+const modalbg = document.querySelector(".bground");  //bground représente la div contenant le formulaire
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalClose = document.getElementsByClassName("close")[0];
+console.log(modalClose);
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
+  console.log("fonction afficher");
   modalbg.style.display = "block";
 }
 
-
+// quitter le formulaire
+modalClose.addEventListener("click", quitModal); 
+function quitModal() {
+  console.log("fonction quitter");
+  modalbg.style.display = "none";
+}
